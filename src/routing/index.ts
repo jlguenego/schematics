8b36cc17369bd7ba83113d1a8017ce781b2c1c7b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-14 09:50:48 +0100

    debugging

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -4,6 +4,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function routing(_options: any): Rule {
+  console.log('hello');
   return (tree: Tree, _context: SchematicContext) => {
     tree.create('AJLG.txt', 'this is a content');
     return tree;
