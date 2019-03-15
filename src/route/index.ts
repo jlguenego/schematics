76636a3446ac3ba51825f10a9e8b499e0cb3e3ac
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-15 10:30:05 +0100

    route in progress

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,10 +1,39 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain, externalSchematic, SchematicsException, schematic } from '@angular-devkit/schematics';
+import { setupOptions } from '../utilities/setupOptions';
+import { findModuleFromOptions } from '@schematics/angular/utility/find-module';
+import { parseName } from '@schematics/angular/utility/parse-name';
 
+const MODULE_EXT = '.module.ts';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function route(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function route(options: any): Rule {
+  // purpose: add a route in a module.
+  // 1) add a new component in the route directory under the module
+  // 2) find the module it has to go in from the path
+  // 3) create a routing module if needed
+  // 4) add the route in the route variable.
+  return (tree: Tree, context: SchematicContext) => {
+    setupOptions(tree, options);
+    const rules: Rule[] = [];
+
+    rules.push(externalSchematic('@schematics/angular', 'component', options));
+
+    const modulePath = findModuleFromOptions(tree, options);
+    console.log('modulePath', modulePath);
+    
+    if (!modulePath) {
+      throw new SchematicsException(`Looks like the schema.json is not right...`);
+    }
+    
+    const moduleParsedPath = parseName('', modulePath);
+    const moduleName = moduleParsedPath.name.split(MODULE_EXT)[0];
+    console.log('moduleName', moduleName);
+    const routingOptions = {
+      name: moduleName,
+    };
+    rules.push(schematic('routing', routingOptions));
+
+    return chain(rules)(tree, context);
   };
 }
--- src/utilities/setupOptions.ts
+++ src/utilities/setupOptions.ts
@@ -1,10 +1,13 @@
 import { getWorkspace } from '@schematics/angular/utility/config';
 import { buildDefaultPath } from '@schematics/angular/utility/project';
 import { parseName } from '@schematics/angular/utility/parse-name';
-import { Tree } from '@angular-devkit/schematics';
+import { Tree, SchematicsException } from '@angular-devkit/schematics';
 import { WorkspaceProject, ProjectType } from '@schematics/angular/utility/workspace-models';
 
 export function setupOptions(host: Tree, options: any): Tree {
+    if (options.name === undefined) {
+        throw new SchematicsException(`Looks like the schema.json is not right...`);
+    }
     const workspace = getWorkspace(host);
     if (!options.project) {
         options.project = Object.keys(workspace.projects)[0];
@@ -13,8 +16,10 @@ export function setupOptions(host: Tree, options: any): Tree {
     if (options.path === undefined) {
         options.path = buildDefaultPath(<WorkspaceProject<ProjectType.Application>>project);
     }
+
     const parsedPath = parseName(options.path, options.name);
     options.name = parsedPath.name;
     options.path = parsedPath.path;
+
     return host;
 }
