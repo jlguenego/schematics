03d009688668f0f2d02d5879198075bd1dc113e7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-03-16 11:30:31 +0100

    page component

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,10 +1,19 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, SchematicContext, Tree, externalSchematic, chain } from '@angular-devkit/schematics';
+import { Schema as ComponentOptions } from '@schematics/angular/component/schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function page(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function page(options: ComponentOptions): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const rules: Rule[] = [];
+    const createComponentOptions: ComponentOptions = {
+      ...options,
+      name: options.name + '-page',
+      entryComponent: true,
+    };
+
+    rules.push(externalSchematic('@schematics/angular', 'component', createComponentOptions));
+
+    return chain(rules)(tree, context);
   };
 }
